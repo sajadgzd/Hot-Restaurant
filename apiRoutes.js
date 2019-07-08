bcf76217705e47d5af0f52e96be2353e99497e13
Laura-Neff <ljneff@gmail.com>
2019-07-08 11:42:09 -0400

    Some AJAX edits

--- apiRoutes.js
+++ apiRoutes.js
@@ -5,5 +5,11 @@ module.exports = function(app) {
 
     app.get("/api/tables", function(req, res) {
         res.json(Data);
+        for(i = 0; i < reserved.length; i++){
+            $(`<div class="card" id="reservedGuy">`);
+            $(`<h5 id="reserveName">`).attr(reserved[i].name);
+            $("#reserveName").appendTo("#reservedGuy");
+            $("#reservedGuy").appendTo("#ReservationRegion");
+        }  
     });
 }
